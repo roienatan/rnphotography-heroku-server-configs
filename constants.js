d41df9d47f8ec71572f3e705fc0b25fcbd9b16a8
Roie Natan <roie.natan@gmail.com>
2019-08-16 02:39:14 +0300

    init heroku server configs

---
+++ constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DB_URL: 'mongodb://heroku_x9smf37z:ukhhgvm25m265j6cmfhkanphr3@ds163517.mlab.com:63517/heroku_x9smf37z',
+    DB_NAME: 'heroku_x9smf37z',
+    ADMIN: {
+        username: 'RoieNatan',
+        password: '12345'
+    }
+}
